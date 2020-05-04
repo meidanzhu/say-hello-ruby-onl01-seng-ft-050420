f72a7cad2343ddf76aedff346bad06e413717d2a
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-05-04 21:37:29 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,8 +1,5 @@
 # Build your say_hello method here
-def say_hello(name)
- puts "Hello #{name}!"
-end
 
-def say_hello(name = "Ruby programmer")
-  puts "Hello, #{name}"
+def say_hello(name = "Ruby Programmer!")
+  puts "Hello #{name}"
 end
